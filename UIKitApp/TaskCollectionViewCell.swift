aada029866cd1ae1a0a10d647c9df828d96543f4
Lucas de Oliveira Carvalho <lucas.carvalho@icomp.ufam.edu.br>
2020-06-16 10:52:33 -0400

    Classe de controle da CollectionView criada

---
+++ UIKitApp/TaskCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaskCollectionViewCell.swift
+//  UIKitApp
+//
+//  Created by Lucas Carvalho on 16/06/20.
+//  Copyright © 2020 Lucas Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class TaskCollectionViewCell: UICollectionViewCell {
+    
+}
