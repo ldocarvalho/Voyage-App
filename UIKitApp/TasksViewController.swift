7d0f1a5d9a0b9e6e29590b3c4c286ff5cfed2767
Lucas de Oliveira Carvalho <lucas.carvalho@icomp.ufam.edu.br>
2020-06-16 10:46:50 -0400

    View Controller adicionada ao segundo storyboard

---
+++ UIKitApp/TasksViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TasksViewController.swift
+//  UIKitApp
+//
+//  Created by Lucas Carvalho on 16/06/20.
+//  Copyright © 2020 Lucas Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+    
+}
