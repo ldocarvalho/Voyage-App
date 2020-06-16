7a89c07626bcf88cb7d5b3f63361f7b243a144a6
Lucas de Oliveira Carvalho <lucas.carvalho@icomp.ufam.edu.br>
2020-06-16 10:56:47 -0400

    Controles adicionados à View Controller do segundo storyboard

--- UIKitApp/TasksViewController.swift
+++ UIKitApp/TasksViewController.swift
@@ -9,5 +9,58 @@
 import UIKit
 
 class TasksViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var restartButton: UIButton!
     
+    var tasks = Task.fetchTasks()
+    
+    let cellScale: CGFloat = 0.6
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Adjusting the controls to the text view
+        
+        // Layout adjustment
+        let screenSize = UIScreen.main.bounds.size
+        let cellWidth = floor(screenSize.width * cellScale)
+        let cellHeight = floor(screenSize.height * cellScale)
+        let insetX = (view.bounds.width - cellWidth) / 2.0
+        let insetY = (view.bounds.height - cellWidth) / 2.0
+        
+        let layout = collectionView!.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        layout.sectionInset = UIEdgeInsets(top: 0.0, left: insetX, bottom: insetY, right: insetX)
+        
+        // Selecting the ViewController class to be the data source for the collection view
+        collectionView.dataSource = self
+    }
+    
+    // Adding the alert dialog to the button
+    @IBAction func restartGoal(_ sender: Any) {
+        let alert = UIAlertController(title: "Reiniciar viagem", message: "Você tem certeza que deseja reiniciar sua viagem com outra meta?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Sim", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Não", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true)
+    }
+}
+
+// Collecting data source and adding to the cells in the Collection View
+extension TasksViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TaskCollectionViewCell", for: indexPath) as! TaskCollectionViewCell
+        let task = tasks[indexPath.item]
+        cell.task = task
+        return cell
+    }
 }
